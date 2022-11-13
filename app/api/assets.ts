5f20554fc7273f2fa175417b0b8a4c59076f8e36
Daniil Voshchuk <vochshukdaniil@gmail.com>
2022-11-13 21:33:21 +0300

    Add new api requests

--- app/api/assets.ts
+++ app/api/assets.ts
@@ -1,6 +1,34 @@
-import type { Assets } from '~/types/assets';
+import type { Asset, AssetHistoryStage, Assets } from '~/types/assets';
 
 export const getThreeAssets: () => Promise<Assets> = async () => {
   const response = await fetch('https://api.coincap.io/v2/assets?limit=3');
+  return response.json();
+};
+
+export const getTableAssets: (
+  page: number,
+  itemsPerPage: number
+) => Promise<Assets> = async (page, itemsPerPage) => {
+  const response = await fetch(
+    `https://api.coincap.io/v2/assets?limit=${itemsPerPage}&offset=${
+      (page - 1) * itemsPerPage
+    }`
+  );
+  return response.json();
+};
+
+export const getAsset: (id: string) => Promise<{ data: Asset }> = async (
+  id
+) => {
+  const response = await fetch(`https://api.coincap.io/v2/assets/${id}`);
+  return response.json();
+};
+
+export const getAssetHistory: (id: string) => Promise<{
+  data: AssetHistoryStage[];
+}> = async (id) => {
+  const response = await fetch(
+    `https://api.coincap.io/v2/assets/${id}/history?interval=h1`
+  );
   return response.json();
 };
