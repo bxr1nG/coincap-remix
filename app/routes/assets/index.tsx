7bcf594bb0ae772bf179c55612a7a4c6b64306d7
Daniil Voshchuk <vochshukdaniil@gmail.com>
2022-11-13 21:37:58 +0300

    Add assets page to route

--- app/routes/assets/index.tsx
+++ app/routes/assets/index.tsx
@@ -1,7 +1,42 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import type { Assets, TableAsset } from '~/types/assets';
+import { getTableAssets } from '~/api/assets';
+import { tableAssetDto } from '~/dtos/table-asset-dto';
+import AssetsPage from '~/pages/Assets/Assets';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get('page');
+
+  if (!page) {
+    return redirect('/assets?page=1');
+  }
+
+  const itemsPerPage = process.env.ITEMS_PER_PAGE
+    ? +process.env.ITEMS_PER_PAGE
+    : 25;
+  const tableAssets: Assets = await getTableAssets(+page, itemsPerPage);
+  return {
+    assets: tableAssets.data.map(tableAssetDto),
+    page: +page,
+    itemsPerPage
+  };
+};
+
 export default function AssetsIndex() {
+  const data = useLoaderData<{
+    assets: TableAsset[];
+    page: number;
+    itemsPerPage: number;
+  }>();
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <AssetsPage
+      assets={data.assets}
+      page={data.page}
+      itemsPerPage={data.itemsPerPage}
+    />
   );
 }
