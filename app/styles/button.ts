ce88e5da311fefc76144716aacd884b4bec1198e
Daniil Voshchuk <vochshukdaniil@gmail.com>
2022-11-24 19:17:10 +0300

    Refactor global components styles

--- app/styles/button.ts
+++ app/styles/button.ts
@@ -3,19 +3,25 @@ import styled from 'styled-components';
 export const Button = styled('button')<{
   secondary?: boolean;
   large?: boolean;
+  disabled?: boolean;
 }>`
-  background-color: ${(props) =>
-    props.secondary
-      ? props.theme.palette.secondary.main
-      : props.theme.palette.primary.main};
+  background-color: ${(props) => {
+    if (props.disabled) {
+      return props.theme.palette.common.gray;
+    }
+    if (props.secondary) {
+      return props.theme.palette.secondary.main;
+    }
+    return props.theme.palette.primary.main;
+  }};
   color: ${(props) => props.theme.palette.primary.contrastText};
   border: none;
   border-radius: 2rem;
   padding: ${(props) => (props.large ? '1rem 2rem' : '0.5rem 1rem')};
   font: ${(props) => props.theme.font.heading};
-  cursor: pointer;
+  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
   transition: box-shadow 0.2s ease-in-out;
   &:hover {
-    box-shadow: ${(props) => props.theme.shadow};
+    box-shadow: ${(props) => (props.disabled ? '' : props.theme.shadow)};
   }
 `;
--- app/styles/input.ts
+++ app/styles/input.ts
@@ -25,7 +25,6 @@ export const Input = styled('input')<{
   }
 
   ${(props) =>
-    // eslint-disable-next-line operator-linebreak
     props.hideArrows &&
     `
     &::-webkit-outer-spin-button,
