cbf7671d443227b8bb773566e2eedacef528dac2
Daniil Voshchuk <vochshukdaniil@gmail.com>
2022-11-26 17:49:42 +0300

    Create portfolio table component

---
+++ app/components/PortfolioTable/PortfolioTable.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+
+import type { PortfolioAsset, PortfolioSummary } from '~/types/assets';
+import { moneyFormatter, percentageFormatter } from '~/utils/numberFormatter';
+import PortfolioConfirmationModal from '~/components/PortfolioConfirmationModal/PortfolioConfirmationModal';
+import { Button } from '~/styles/button';
+import {
+  Table as StyledTable,
+  Td,
+  PercentTd,
+  Thead,
+  ClickableTr
+} from '~/styles/table';
+
+import { COLUMNS } from './constants';
+
+function PortfolioTable(props: {
+  portfolioData: {
+    assets: PortfolioAsset[];
+    summary: PortfolioSummary;
+  };
+}) {
+  const { portfolioData } = props;
+
+  const [confirmationAsset, setConfirmationAsset] =
+    useState<PortfolioAsset | null>(null);
+
+  return (
+    <>
+      <StyledTable simpleStyle maxHeight="70%">
+        <Thead>
+          <tr>
+            {COLUMNS.map((column) => (
+              <Td
+                key={column.name}
+                as="th"
+                textAlign={column.textAlign}
+                hideMobile={column.hideMobile}
+              >
+                {column.name}
+              </Td>
+            ))}
+          </tr>
+        </Thead>
+        <tbody>
+          {portfolioData.assets.map((asset) => (
+            <ClickableTr
+              key={asset.dateId}
+              onClick={() => {
+                setConfirmationAsset(asset);
+              }}
+            >
+              <Td textAlign="left">
+                <div>{asset.name}</div>
+              </Td>
+              <Td textAlign="right">{asset.amount}</Td>
+              <Td textAlign="right" hideMobile>
+                {moneyFormatter(asset.price)}
+              </Td>
+              <Td textAlign="right" hideMobile>
+                {moneyFormatter(asset.total)}
+              </Td>
+              <Td textAlign="right" hideMobile>
+                {moneyFormatter(asset.currentPrice)}
+              </Td>
+              <Td textAlign="right">{moneyFormatter(asset.currentTotal)}</Td>
+              <PercentTd textAlign="right" changeSign={asset.change >= 0}>
+                {percentageFormatter(asset.change, true)}
+              </PercentTd>
+              <Td textAlign="center" hideMobile>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setConfirmationAsset(asset);
+                  }}
+                >
+                  &#9587;
+                </Button>
+              </Td>
+            </ClickableTr>
+          ))}
+        </tbody>
+      </StyledTable>
+      <PortfolioConfirmationModal
+        confirmationAsset={confirmationAsset}
+        setConfirmationAsset={setConfirmationAsset}
+      />
+    </>
+  );
+}
+
+export default PortfolioTable;
---
+++ app/components/PortfolioTable/constants.ts
@@ -0,0 +1,42 @@
+export const COLUMNS = [
+  {
+    name: 'Name',
+    textAlign: 'left',
+    hideMobile: false
+  },
+  {
+    name: 'Amount',
+    textAlign: 'right',
+    hideMobile: false
+  },
+  {
+    name: 'Initial Price',
+    textAlign: 'right',
+    hideMobile: true
+  },
+  {
+    name: 'Total',
+    textAlign: 'right',
+    hideMobile: true
+  },
+  {
+    name: 'Current Price',
+    textAlign: 'right',
+    hideMobile: true
+  },
+  {
+    name: 'Current Total',
+    textAlign: 'right',
+    hideMobile: false
+  },
+  {
+    name: 'Change',
+    textAlign: 'right',
+    hideMobile: false
+  },
+  {
+    name: 'Buy',
+    textAlign: 'center',
+    hideMobile: true
+  }
+];
